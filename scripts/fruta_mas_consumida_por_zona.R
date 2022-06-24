0fab3a90e7379b8736c04a2e65c19f8942ad005c
armandios19 <fooseaov@gmail.com>
2022-06-24 11:39:17 -0500

    se normalizo la cdmx

--- scripts/fruta_mas_consumida_por_zona.R
+++ scripts/fruta_mas_consumida_por_zona.R
@@ -264,4 +264,36 @@ ensanut_100k %>%
 
 # cdmx
 
+ensanut_100k %>%
+  filter(categoria == "frutas") %>%
+  select(descripcion_entidad, descripcion_municipio, descripcion_localidad,
+         sexo, numero_alimento, dias_comio, categoria, region_nutricion,
+         area, edad_categorica) %>%
+  filter(region_nutricion == "cdmx") %>%
+  select(dias_comio, edad_categorica, numero_alimento) %>%
+  mutate(consumo = ifelse(dias_comio=="0", 0,1)) %>%
+  filter(consumo == 1) %>%
+  group_by(numero_alimento, consumo) %>%
+  count() %>%
+  mutate(porcentaje = n/1873) %>%
+  ggplot(aes(x = numero_alimento, y = porcentaje, fill = numero_alimento)) +
+  xlab("Frutas") +
+  ylab("Porcentaje") +
+  geom_col() +
+  theme_classic() +
+  theme(legend.position = "none")+
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))
+
+ensanut_100k %>%
+  filter(categoria == "frutas") %>%
+  select(descripcion_entidad, descripcion_municipio, descripcion_localidad,
+         sexo, numero_alimento, dias_comio, categoria, region_nutricion,
+         area, edad_categorica) %>%
+  filter(region_nutricion == "cdmx") %>%
+  select(dias_comio, edad_categorica, numero_alimento) %>%
+  mutate(consumo = ifelse(dias_comio=="0", 0,1)) %>%
+  filter(consumo == 1) %>%
+  group_by(numero_alimento, consumo) %>%
+  count() %>%
+  mutate(porcentaje = n/1873)
 
