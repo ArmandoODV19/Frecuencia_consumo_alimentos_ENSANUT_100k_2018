adc925122fd041dfa78b3d8ef083ec5694051208
armandios19 <fooseaov@gmail.com>
2022-07-05 21:43:59 -0500

    agregando ceros

--- scripts/les_nines.R
+++ scripts/les_nines.R
@@ -268,3 +268,190 @@ ensanut_100k %>%
   summarise(total = sum(n))
 
 
+### agregando los ceros ###
+
+# general
+
+
+
+ensanut_100k %>%
+  filter(tamano_porcion %in% c("C", "E", "G", "M")) %>%
+  select(descripcion_entidad, descripcion_municipio, descripcion_localidad,
+         numero_alimento, dias_comio, tamano_porcion, categoria, region_nutricion,
+         area, edad_categorica) %>%
+  select(categoria, dias_comio) %>%
+  group_by(categoria, dias_comio) %>%
+  count() %>%
+  mutate(porcentaje = case_when(categoria == "bebidas" ~ n/51240,
+                                categoria == "botanas_dulces_postres" ~ n/23608,
+                                categoria == "carnes_embutido_huevo" ~ n/32858,
+                                categoria == "cereales_tuberculos" ~ n/26553,
+                                categoria == "comida_rapida" ~ n/4434,
+                                categoria == "frutas" ~ n/33736,
+                                categoria == "lacteos" ~ n/23973,
+                                categoria == "leguminosas" ~ n/17356,
+                                categoria == "miscelaneos" ~ n/4158,
+                                categoria == "pescado_mariscos" ~ n/4522,
+                                categoria == "productos_maiz" ~ n/12454,
+                                categoria == "sopás_cremas_pastas" ~ n/17933,
+                                categoria == "suplementos" ~ n/1654,
+                                categoria == "verduras" ~ n/34699,)) %>%
+  ggplot(aes(x = categoria, y = porcentaje, fill = categoria)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = dias_comio), size = 5)+
+  xlab("alimento")+
+  ylab("consumo")+
+  theme_classic()+
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+  guides(fill = FALSE)
+
+
+ensanut_100k %>%
+  filter(tamano_porcion %in% c("C", "E", "G", "M"),
+         categoria == "verduras") %>%
+  select(categoria, dias_comio) %>%
+  group_by(categoria, dias_comio) %>%
+  count() %>%
+  ungroup(categoria, dias_comio) %>%
+  summarise(total = sum(n))
+
+# zona norte
+
+ensanut_100k %>%
+  filter(tamano_porcion %in% c("C", "E", "G", "M"),
+         region_nutricion == "norte") %>%
+  select(descripcion_entidad, descripcion_municipio, descripcion_localidad,
+         numero_alimento, dias_comio, tamano_porcion, categoria, region_nutricion,
+         area, edad_categorica) %>%
+  select(categoria, dias_comio) %>%
+  group_by(categoria, dias_comio) %>%
+  count() %>%
+  mutate(porcentaje = case_when(categoria == "bebidas" ~ n/7774,
+                                categoria == "botanas_dulces_postres" ~ n/4343,
+                                categoria == "carnes_embutido_huevo" ~ n/5602,
+                                categoria == "cereales_tuberculos" ~ n/4672,
+                                categoria == "comida_rapida" ~ n/1124,
+                                categoria == "frutas" ~ n/4483,
+                                categoria == "lacteos" ~ n/3563,
+                                categoria == "leguminosas" ~ n/2772,
+                                categoria == "miscelaneos" ~ n/786,
+                                categoria == "pescado_mariscos" ~ n/630,
+                                categoria == "productos_maiz" ~ n/1461,
+                                categoria == "sopás_cremas_pastas" ~ n/2593,
+                                categoria == "suplementos" ~ n/181,
+                                categoria == "verduras" ~ n/5151,)) %>%
+  ggplot(aes(x = categoria, y = porcentaje, fill = categoria)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = dias_comio), size = 5)+
+  xlab("alimento")+
+  ylab("consumo")+
+  theme_classic()+
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+  guides(fill = FALSE)
+
+
+ensanut_100k %>%
+  filter(tamano_porcion %in% c("C", "E", "G", "M"),
+         region_nutricion == "norte",
+         categoria == "verduras") %>%
+  select(categoria, dias_comio) %>%
+  group_by(categoria, dias_comio) %>%
+  count() %>%
+  ungroup(categoria, dias_comio) %>%
+  summarise(total = sum(n))
+
+
+
+## zona sur
+
+
+ensanut_100k %>%
+  filter(tamano_porcion %in% c("C", "E", "G", "M"),
+         region_nutricion == "sur") %>%
+  select(descripcion_entidad, descripcion_municipio, descripcion_localidad,
+         numero_alimento, dias_comio, tamano_porcion, categoria, region_nutricion,
+         area, edad_categorica) %>%
+  select(categoria, dias_comio) %>%
+  group_by(categoria, dias_comio) %>%
+  count() %>%
+  mutate(porcentaje = case_when(categoria == "bebidas" ~ n/25110,
+                                categoria == "botanas_dulces_postres" ~ n/9173,
+                                categoria == "carnes_embutido_huevo" ~ n/14992,
+                                categoria == "cereales_tuberculos" ~ n/11228,
+                                categoria == "comida_rapida" ~ n/1343,
+                                categoria == "frutas" ~ n/15958,
+                                categoria == "lacteos" ~ n/10656,
+                                categoria == "leguminosas" ~ n/8433,
+                                categoria == "miscelaneos" ~ n/1606,
+                                categoria == "pescado_mariscos" ~ n/2354,
+                                categoria == "productos_maiz" ~ n/6793,
+                                categoria == "sopás_cremas_pastas" ~ n/8986,
+                                categoria == "suplementos" ~ n/901,
+                                categoria == "verduras" ~ n/14733,)) %>%
+  ggplot(aes(x = categoria, y = porcentaje, fill = categoria)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = dias_comio), size = 5)+
+  xlab("alimento")+
+  ylab("consumo")+
+  theme_classic()+
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+  guides(fill = FALSE)
+
+
+ensanut_100k %>%
+  filter(tamano_porcion %in% c("C", "E", "G", "M"),
+         region_nutricion == "sur",
+         categoria == "verduras") %>%
+  select(categoria, dias_comio) %>%
+  group_by(categoria, dias_comio) %>%
+  count() %>%
+  ungroup(categoria, dias_comio) %>%
+  summarise(total = sum(n))
+
+
+### zona centro
+
+ensanut_100k %>%
+  filter(tamano_porcion %in% c("C", "E", "G", "M"),
+         region_nutricion == "centro") %>%
+  select(descripcion_entidad, descripcion_municipio, descripcion_localidad,
+         numero_alimento, dias_comio, tamano_porcion, categoria, region_nutricion,
+         area, edad_categorica) %>%
+  select(categoria, dias_comio) %>%
+  group_by(categoria, dias_comio) %>%
+  count() %>%
+  mutate(porcentaje = case_when(categoria == "bebidas" ~ n/15963,
+                                categoria == "botanas_dulces_postres" ~ n/8673,
+                                categoria == "carnes_embutido_huevo" ~ n/10568,
+                                categoria == "cereales_tuberculos" ~ n/9224,
+                                categoria == "comida_rapida" ~ n/1629,
+                                categoria == "frutas" ~ n/11422,
+                                categoria == "lacteos" ~ n/8309,
+                                categoria == "leguminosas" ~ n/5456,
+                                categoria == "miscelaneos" ~ n/1480,
+                                categoria == "pescado_mariscos" ~ n/1326,
+                                categoria == "productos_maiz" ~ n/3717,
+                                categoria == "sopás_cremas_pastas" ~ n/5481,
+                                categoria == "suplementos" ~ n/531,
+                                categoria == "verduras" ~ n/12638)) %>%
+  ggplot(aes(x = categoria, y = porcentaje, fill = categoria)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = dias_comio), size = 5)+
+  xlab("alimento")+
+  ylab("consumo")+
+  theme_classic()+
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+  guides(fill = FALSE)
+
+
+ensanut_100k %>%
+  filter(tamano_porcion %in% c("C", "E", "G", "M"),
+         region_nutricion == "centro",
+         categoria == "verduras") %>%
+  select(categoria, dias_comio) %>%
+  group_by(categoria, dias_comio) %>%
+  count() %>%
+  ungroup(categoria, dias_comio) %>%
+  summarise(total = sum(n))
+
+
